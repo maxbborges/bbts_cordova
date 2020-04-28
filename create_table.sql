100fbb52573ccbf5597d7dafc2af9b2e67ca8583
Luiz Paulo <luizhat10@gmail.com>
2020-04-28 19:40:45 -0300

    modificacao de tabelas e inserindo funcionarios

--- create_table.sql
+++ create_table.sql
@@ -1,4 +1,60 @@
-CREATE TABLE funcionario (matricula int NOT NULL PRIMARY KEY,nome varchar(255),senha varchar(255), chave_c varchar(255),email varchar(255), telefone int, contratacao varchar(255),ferias_restantes int);
-CREATE TABLE atributos (id int NOT NULL AUTO_INCREMENT PRIMARY KEY,matricula_funcionario int, atributo varchar(255),FOREIGN KEY (matricula_funcionario) REFERENCES funcionario(matricula));
-CREATE TABLE ferias (id int NOT NULL AUTO_INCREMENT PRIMARY KEY, funcionario varchar(255), data_inicial varchar(255), data_final varchar(255), data_solicitacao varchar(255), num_abono int, adiantamento int,status varchar(255));
-CREATE TABLE abonos (id int NOT NULL AUTO_INCREMENT PRIMARY KEY, funcionario varchar(255), data_inicial varchar(255), data_final varchar(255), data_solicitacao varchar(255));
+-- CREATE TABLE funcionario (matricula int NOT NULL PRIMARY KEY,nome varchar(255),senha varchar(255), chave_c varchar(255),email varchar(255), telefone int, contratacao varchar(255),ferias_restantes int);
+-- CREATE TABLE atributos (id int NOT NULL AUTO_INCREMENT PRIMARY KEY,matricula_funcionario int, atributo varchar(255),FOREIGN KEY (matricula_funcionario) REFERENCES funcionario(matricula));
+-- CREATE TABLE ferias (id int NOT NULL AUTO_INCREMENT PRIMARY KEY, funcionario varchar(255), data_inicial varchar(255), data_final varchar(255), data_solicitacao varchar(255), num_abono int, adiantamento int,status varchar(255));
+-- CREATE TABLE abonos (id int NOT NULL AUTO_INCREMENT PRIMARY KEY, funcionario varchar(255), data_inicial varchar(255), data_final varchar(255), data_solicitacao varchar(255));
+
+
+DROP TABLE banco_bbts.abono;
+DROP TABLE banco_bbts.atributos;
+DROP TABLE banco_bbts.ferias;
+DROP TABLE banco_bbts.funcionario;
+
+CREATE TABLE `funcionario` (
+  `matricula` int(11) NOT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `senha` varchar(255) DEFAULT NULL,
+  `chave_c` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `ferias_restantes` int(11) DEFAULT NULL,
+  `cpf` varchar(255) DEFAULT NULL,
+  `rg` varchar(255) DEFAULT NULL,
+  `cartao_bb` varchar(255) DEFAULT NULL,
+  `cartao_capital` varchar(255) DEFAULT NULL,
+  `num_contrato` varchar(255) DEFAULT NULL,
+  `contratacao` date DEFAULT NULL,
+  PRIMARY KEY (`matricula`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `atributos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `matricula_funcionario` int(11) DEFAULT NULL,
+  `atributo` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `matricula_funcionario` (`matricula_funcionario`),
+  CONSTRAINT `atributos_ibfk_1` FOREIGN KEY (`matricula_funcionario`) REFERENCES `funcionario` (`matricula`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `ferias` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `matricula_funcionarios` varchar(255) DEFAULT NULL,
+  `num_abono` int(11) DEFAULT NULL,
+  `adiantamento` int(11) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `data_inicial` date DEFAULT NULL,
+  `data_final` date DEFAULT NULL,
+  `data_solicitacao` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+  KEY `matricula_funcionario` (`matricula_funcionario`),
+  CONSTRAINT `atributos_ibfk_1` FOREIGN KEY (`matricula_funcionario`) REFERENCES `funcionario` (`matricula`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `abonos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `matricula_funcionario` varchar(255) DEFAULT NULL,
+  `data_final` date DEFAULT NULL,
+  `data_inicial` date DEFAULT NULL,
+  `data_solicitacao` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+  KEY `matricula_funcionario` (`matricula_funcionario`),
+  CONSTRAINT `atributos_ibfk_1` FOREIGN KEY (`matricula_funcionario`) REFERENCES `funcionario` (`matricula`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- insert_into.sql
+++ insert_into.sql
@@ -1,10 +1,43 @@
-INSERT INTO funcionario (matricula,nome,senha,chave_c,email,telefone,contratacao,ferias_restantes) values (1,'maxwell', md5(12345),'xxxx','max@bbts.com.br','99999999','19/11/2000',30);
-INSERT INTO funcionario (matricula,nome,senha,chave_c,email,telefone,contratacao,ferias_restantes) values (2,'luiz', md5(12345),'xxxx','luiz@bbts.com.br','88888888','19/11/2000',30);
-INSERT INTO funcionario (matricula,nome,senha,chave_c,email,telefone,contratacao,ferias_restantes) values (3,'abdiel', md5(12345),'c1280576','abdiel@bbts.com.br','77777777','19/11/2000',30);
-INSERT INTO funcionario (matricula,nome,senha,chave_c,email,telefone,contratacao,ferias_restantes) values (4,'Alexandre', md5(12345),'xxxx','Alexandre@bbts.com.br','66666666','19/11/2000',30);
-INSERT INTO funcionario (matricula,nome,senha,chave_c,email,telefone,contratacao,ferias_restantes) values (5,'aline', md5(12345),'xxxx','aline@bbts.com.br','55555555','19/11/2000',30);
-INSERT INTO funcionario (matricula,nome,senha,chave_c,email,telefone,contratacao,ferias_restantes) values (6,'carla', md5(12345),'xxxx','carla@bbts.com.br','44444444','19/11/2000',30);
+-- INSERT INTO funcionario (matricula,nome,senha,chave_c,email,telefone,contratacao,ferias_restantes) values (1,'maxwell', md5(12345),'xxxx','max@bbts.com.br','99999999','19/11/2000',30);
+-- INSERT INTO funcionario (matricula,nome,senha,chave_c,email,telefone,contratacao,ferias_restantes) values (2,'luiz', md5(12345),'xxxx','luiz@bbts.com.br','88888888','19/11/2000',30);
+-- INSERT INTO funcionario (matricula,nome,senha,chave_c,email,telefone,contratacao,ferias_restantes) values (3,'abdiel', md5(12345),'c1280576','abdiel@bbts.com.br','77777777','19/11/2000',30);
+-- INSERT INTO funcionario (matricula,nome,senha,chave_c,email,telefone,contratacao,ferias_restantes) values (4,'Alexandre', md5(12345),'xxxx','Alexandre@bbts.com.br','66666666','19/11/2000',30);
+-- INSERT INTO funcionario (matricula,nome,senha,chave_c,email,telefone,contratacao,ferias_restantes) values (5,'aline', md5(12345),'xxxx','aline@bbts.com.br','55555555','19/11/2000',30);
+-- INSERT INTO funcionario (matricula,nome,senha,chave_c,email,telefone,contratacao,ferias_restantes) values (6,'carla', md5(12345),'xxxx','carla@bbts.com.br','44444444','19/11/2000',30);
+--
+--
+-- INSERT INTO atributos (matricula_funcionario,atributo) values (1,'administrativo');
+-- INSERT INTO atributos (matricula_funcionario,atributo) values (1,'tecnico');
+-- INSERT INTO atributos (matricula_funcionario,atributo) values (2,'tecnico');
 
-INSERT INTO atributos (matricula_funcionario,atributo) values (1,'administrativo');
-INSERT INTO atributos (matricula_funcionario,atributo) values (1,'tecnico');
-INSERT INTO atributos (matricula_funcionario,atributo) values (2,'tecnico');
+
+
+INSERT INTO funcionario (matricula, nome, senha, chave_c, email, ferias_restantes, cpf, rg, cartao_bb, cartao_capital, num_contrato, contratacao)
+values (109712,'FERNANDO DA SILVA FARIAS', md5(12345),'C1271754','fernando.farias@bbts.com.br'
+,30,'01797043110','2612739 sspdf',
+'99999','99999','202085580001','2013-08-09');
+
+INSERT INTO funcionario (matricula, nome, senha, chave_c, email, ferias_restantes, cpf, rg, cartao_bb, cartao_capital, num_contrato, contratacao)
+values (106847,'GUILHERME GOMES SOARES DA SILVA', md5(12345),'C1259067','guilhermegomes.silva@bbts.com.br'
+,30,'03618661193','8091497 sspdf',
+'99999','99999','202085580001','2012-03-01');
+
+INSERT INTO funcionario (matricula, nome, senha, chave_c, email, ferias_restantes, cpf, rg, cartao_bb, cartao_capital, num_contrato, contratacao)
+values (105928,'HARITON DOS SANTOS RIBEIRO', md5(12345),'C1310326','hariton.ribeiro@bbts.com.br'
+,30,'05057180908','93850017 sspdf',
+'99999','99999','202085580001','2011-11-01');
+
+INSERT INTO funcionario (matricula, nome, senha, chave_c, email, ferias_restantes, cpf, rg, cartao_bb, cartao_capital, num_contrato, contratacao)
+values (110245,'HELDER FRANCISCO MARTINS', md5(12345),'C1275740','helder.martins@bbts.com.br'
+,30,'15014806856','2037188 sspdf',
+'99999','99999','202085580001','2014-01-02');
+
+INSERT INTO funcionario (matricula, nome, senha, chave_c, email, ferias_restantes, cpf, rg, cartao_bb, cartao_capital, num_contrato, contratacao)
+values (111322,'JACY ALVES DA COSTA JUNIOR', md5(12345),'C1280353','jacy.costa@bbts.com.br'
+,30,'71218521104','1994163 sspdf',
+'99999','99999','202085580001','2014-11-21');
+
+INSERT INTO funcionario (matricula, nome, senha, chave_c, email, ferias_restantes, cpf, rg, cartao_bb, cartao_capital, num_contrato, contratacao)
+values (111988,'JOAO AUGUSTO BARBOSA BE', md5(12345),'C1282480','joao.be@bbts.com.br'
+,30,'03746280176','262591 sspdf',
+'99999','99999','202085580001','2015-05-19');
