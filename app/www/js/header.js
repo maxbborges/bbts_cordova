a20ff32bb74f09d8cd8912f633f2aa6df91000dd
Luiz Paulo <luizhat10@gmail.com>
2020-04-29 19:16:04 -0300

    atualizacao frontend

--- app/www/js/header.js
+++ app/www/js/header.js
@@ -19,3 +19,16 @@ $( document ).ready(function() {
     });
   }
 });
+
+
+function w3_open() {
+  document.getElementsByClassName("container")[0].style.marginLeft = "25%";
+  document.getElementById("mySidebar").style.width = "25%";
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
+}
+function w3_close() {
+  document.getElementsByClassName("container")[0].style.marginLeft = "0%";
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
+}
--- insert_into.sql
+++ insert_into.sql
@@ -45,3 +45,60 @@ values (111988,'JOAO AUGUSTO BARBOSA BE', md5(12345),'C1282480','joao.be@bbts.co
 
 INSERT INTO atributos (matricula_funcionario,atributo) values (105928,'administrativo');
 INSERT INTO atributos (matricula_funcionario,atributo) values (105928,'tecnico');
+
+################################################# Novos Cadastros #################################################
+
+INSERT INTO funcionario (matricula, nome, senha, chave_c, email, ferias_restantes, cpf, rg, cartao_bb, cartao_capital, num_contrato, contratacao)
+values (111257,'ABDIEL NUNES PEREIRA', md5(12345),'C1280576','abdiel.pereira@bbts.com.br'
+,30,'70013667149','1985955 sspdf',
+'99999','99999','202085580001','2014-12-01');
+
+INSERT INTO funcionario (matricula, nome, senha, chave_c, email, ferias_restantes, cpf, rg, cartao_bb, cartao_capital, num_contrato, contratacao)
+values (107885,'ADRIANO NUNES', md5(12345),'C1262011','adriano.nunes@bbts.com.br'
+,30,'75010100649','1569416 sspdf',
+'99999','99999','202085580001','2012-08-13');
+
+INSERT INTO funcionario (matricula, nome, senha, chave_c, email, ferias_restantes, cpf, rg, cartao_bb, cartao_capital, num_contrato, contratacao)
+values (107760,'ALEXANDRE DE SOUZA SILVA', md5(12345),'C1261565','asouza.silva@bbts.com.br'
+,30,'02843488648','1564257 sspdf',
+'99999','99999','202085580001','2012-07-12');
+
+INSERT INTO funcionario (matricula, nome, senha, chave_c, email, ferias_restantes, cpf, rg, cartao_bb, cartao_capital, num_contrato, contratacao)
+values (111949,'CLAUDIO LUIZ CORREIA RORIZ', md5(12345),'C1000320','claudio.roriz@bbts.com.br'
+,30,'37599968172','951723 sspdf',
+'99999','99999','202085580001','2015-05-06');
+
+INSERT INTO funcionario (matricula, nome, senha, chave_c, email, ferias_restantes, cpf, rg, cartao_bb, cartao_capital, num_contrato, contratacao)
+values (110080,'CLEBERTH CARVALHO CRUZ DA ROCHA', md5(12345),'C1314143','cleberth.rocha@bbts.com.br'
+,30,'02171101180','2653052 sspdf',
+'99999','99999','202085580001','2020-02-14');
+
+INSERT INTO funcionario (matricula, nome, senha, chave_c, email, ferias_restantes, cpf, rg, cartao_bb, cartao_capital, num_contrato, contratacao)
+values (105504,'DANILO ARAUJO REGO', md5(12345),'C1252733','danilo.araujo@bbts.com.br'
+,30,'01533368112','2335135 sspdf',
+'99999','99999','202085580001','2011-09-05');
+
+INSERT INTO funcionario (matricula, nome, senha, chave_c, email, ferias_restantes, cpf, rg, cartao_bb, cartao_capital, num_contrato, contratacao)
+values (112004,'DANILO LOPES FONSECA', md5(12345),'C1252733','danilo.araujo@bbts.com.br'
+,30,'01533368112','2335135 sspmg',
+'99999','99999','202085580001','2011-09-05');
+
+INSERT INTO funcionario (matricula, nome, senha, chave_c, email, ferias_restantes, cpf, rg, cartao_bb, cartao_capital, num_contrato, contratacao)
+values (106847,'GUILHERME GOMES SOARES DA SILVA', md5(12345),'C1282803','danilo.fonseca@bbts.com.br'
+,30,'07483994614','13028465 sspdf',
+'99999','99999','202085580001','2015-06-02');
+
+INSERT INTO funcionario (matricula, nome, senha, chave_c, email, ferias_restantes, cpf, rg, cartao_bb, cartao_capital, num_contrato, contratacao)
+values (111986,'DANIZETE RODRIGUES DO NASCIMENTO', md5(12345),'C1282476','danizete.nascimento@bbts.com.br'
+,30,'00923175105','2403034 sspdf',
+'99999','99999','202085580001','2015-05-19');
+
+INSERT INTO funcionario (matricula, nome, senha, chave_c, email, ferias_restantes, cpf, rg, cartao_bb, cartao_capital, num_contrato, contratacao)
+values (107328,'ELTON SOARES PIRES DE CARVALHO', md5(12345),'C1260257','elton.carvalho@bbts.com.br'
+,30,'69855234120','2175362 sspdf',
+'99999','99999','202085580001','2012-05-02');
+
+INSERT INTO funcionario (matricula, nome, senha, chave_c, email, ferias_restantes, cpf, rg, cartao_bb, cartao_capital, num_contrato, contratacao)
+values (105928,'HARITON DOS SANTOS RIBEIRO', md5(12345),'C1310326','hariton.ribeiro@bbts.com.br'
+,30,'05057180908','93850017 sspdf',
+'99999','99999','202085580001','2011-11-01');
